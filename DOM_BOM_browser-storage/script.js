162f77b21d9fa856a240ed3b31b18e6dcad59cd3
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-01-09 14:21:29 +0300

    add new changes at index.html, script.js

--- DOM_BOM_browser-storage/script.js
+++ DOM_BOM_browser-storage/script.js
@@ -1,24 +1,28 @@
-//let div = document.getElementById("search");
-let elem = document.getElementById("get-weather");
-elem.addEventListener('click', get);
+const elem = document.getElementById("get-weather");
+elem.addEventListener('click', getWeather);
 
-function get(){
-		fetch ("http://api.apixu.com/v1/current.json?key=39e2192ab5be4805b4c190756181912&q=" + document.getElementById('weatherSearch').value)
+const input = document.getElementById('weatherSearch');
+const tbody = document.getElementById('table-body');
+const clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', clearDataTable);
+
+function getWeather(value){
+		fetch ("http://api.apixu.com/v1/current.json?key=39e2192ab5be4805b4c190756181912&q=" + input.value)
 		.then(response => response.json())
 		.then(data => setDataToTable(data), data => console.log(data))
 		.catch(error => console.error(error))
 }
 
+function clearDataTable(){
+	tbody.innerHTML = '';
+}
+
 function setDataToTable(data){
-	let table = document.getElementById('table');
+	let tableBody = document.getElementById('table-body');
 	let tr = document.createElement('tr');
 	let tdCountry = document.createElement('td');
 	let tdCity = document.createElement('td');
 	let tdTemp = document.createElement('td');
-
-	// tdCountry.classList.add('new-table');
-	// tdCity.classList.add('new-table');
-	// tdTemp.classList.add('new-table');
 
 	tdCountry.innerText = data.location.country;
 	tdCity.innerText = data.location.name;
@@ -26,8 +30,30 @@ function setDataToTable(data){
 	tr.appendChild(tdCountry);
 	tr.appendChild(tdCity);
 	tr.appendChild(tdTemp);
-	table.appendChild(tr);
+	tableBody.appendChild(tr);
 }
+
+// function get(){
+// 	fetch ("http://api.apixu.com/v1/current.json?key=39e2192ab5be4805b4c190756181912&q=" + document.getElementById('weatherSearch').value)
+// 	.then(response => response.json())
+// 	.then(data => div.innerHTML = data.location.country + ', ' + data.location.name + ": " + data.current.temp_c + " °C", data => console.log(data))
+// 	.catch(error => console.error(error))
+// }
+
+
+let myLocation = document.getElementById('my-weather');
+myLocation.addEventListener('click', getWeatherByLocation);
+
+function getWeatherByLocation(){
+	navigator.geolocation.getCurrentPosition(({coords}) => {
+		let lat = coords.latitude;
+		let long = coords.longitude;
+		console.log(lat.toFixed(2) + ' ' + long.toFixed(2));
+		getWeather(`${coords.latitude},${coords.longitude}`)
+	});
+}
+
+
 
 
 
