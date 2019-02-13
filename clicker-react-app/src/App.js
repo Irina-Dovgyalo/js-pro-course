47f9fa6d6aa5a504e3a5611f51490d97297ead73
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-02-13 15:07:02 +0300

    create new branch clicker-react-app

---
+++ clicker-react-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Clicker from './components/Clicker';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Clicker />
+      </div>
+    );
+  }
+}
+
+export default App;
