3eb762c33f87502fd335df664ec626e219ad04b0
Irina Dovhgyalo <irinadovgyalo@gmail.com>
2019-02-13 15:45:00 +0300

    create new branch emoji

---
+++ emoji/src/components/EmojiContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import EmojiRow from './EmojiRow';
+
+class EmojiContainer extends Component {
+    render() {
+      return (
+        <EmojiRow />
+      );
+    }
+  }
+  
+  export default EmojiContainer;
+
